a5398d880ee20a8e1f6cff60797073dd216789e6
LianglzNice <572107412@qq.com>
2021-06-24 17:29:20 +0800

    624组件生成基本完成

--- template/html.js
+++ template/html.js
@@ -1,13 +1,24 @@
 module.exports = function(data){
     let dom = '';
     let style = '';
-
+    let script = '';
+    //计算空格
     let sp = (count) => {
         let space = ' ';
         for(let i=0; i<count; i++){
             space += ' ';
         }
         return space;
+    }
+    //列举嵌套组件属性项
+    let getChildAttrs = (attrs, i) => {
+        let childAttrItem = '';
+        if(attrs){ 
+            for(let k=0; k<attrs.length; k++){
+                childAttrItem += `:${attrs[k]}="${i}" `;
+            }
+        }
+        return childAttrItem;
     }
 
     for(let i=0; i<data.length; i++){
@@ -24,9 +35,9 @@ module.exports = function(data){
         //列举属性项
         let attrItem = '';
         for(let n in item.attributes){
-            console.log(typeof item.attributes[n]);
             if(typeof item.attributes[n] === 'boolean'){
                 attrItem += `:${n}="${item.attributes[n]}" `;
+
             }else if(item.attributes[n] !== '' && item.attributes[n] !== null){
                 attrItem += `${n}="${item.attributes[n]}" `;
             }
@@ -37,16 +48,39 @@ module.exports = function(data){
             dom += `${sp(11)}<div class="${item.name}-${i+1}"></div>\r\n`;
             continue;
         }
+
+        let model = item.hasModel ? `v-model="${item.name + (i+1)}"` : '';
+
         dom += `${sp(11)}<div class="${item.name}-${i+1}">\r\n`;
         switch(item.value){
-            case 2:
-                dom += `${sp(15)}<el-${item.name} ${attrItem}>${item.attributes.value}</el-${item.name}>\r\n`;
+            case 2: case 3: case 4:
+                if(item.original.children){
+                    dom += `${sp(15)}<${item.original.name} ${attrItem} ${model}>\r\n`;
+                    for(let m=0; m<2; m++){
+                        let tag = item.attributes.shapeType ? item.attributes.shapeType : item.original.children.name;
+                        dom += `${sp(19)}<${tag} ${getChildAttrs(item.original.children.childAttrs, m+1)}>${item.label+(m+1)}</${tag}>\r\n`;
+                    }
+                    dom += `${sp(15)}</${item.original.name}>\r\n`;
+
+                }else{
+                    dom += `${sp(15)}<${item.original.name} ${attrItem} ${model}>${item.attributes.value}</${item.original.name}>\r\n`;
+                }
                 break;
+
             default: 
-                dom += `${sp(15)}<el-${item.name} ${attrItem}></el-${item.name}>\r\n`;
+                dom += `${sp(15)}<${item.original.name} ${attrItem} ${model}></${item.original.name}>\r\n`;
                 break;
         }
         dom += `${sp(11)}</div>\r\n`;
+
+        //列举script
+        if(item.hasModel){
+            if(item.value === 4){
+                script += `${sp(23)}${item.name + (i+1)} : [1],\r\n`;
+            }else{
+                script += `${sp(23)}${item.name + (i+1)} : '',\r\n`;
+            }
+        }
     }
 
     return (
@@ -70,11 +104,14 @@ ${dom}
         </div>
         <script>
             const { createApp, ref, reactive, toRefs } = Vue;
-            const message = ref('hello');
             const app = createApp({
                 setup() {
+                    const data = reactive({
+${script}
+                    });
+
                     return {
-                        message
+                        ...toRefs(data)
                     }
                 }
             });
